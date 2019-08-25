602e104848a7ac53f05e87281592c0209c81d34f
Igwar <43217217+Igwar@users.noreply.github.com>
2019-08-25 22:39:32 +0300

    Add files via upload

---
+++ FinderLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookLib;
+
+namespace Finder
+{
+    public interface IFinder
+    {
+        Book FindBookByTeg();
+    }
+}
