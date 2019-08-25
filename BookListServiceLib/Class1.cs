38e4002017cd84ec885e621d82e3276de4555a00
Igwar <43217217+Igwar@users.noreply.github.com>
2019-08-25 22:38:30 +0300

    Add files via upload

---
+++ BookListServiceLib/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookLib;
+using BookListStorageLib;
+using Finder;
+namespace BookListServiceLib
+{
+   
+        public class BookService : IBookService
+        {
+             /// <summary>
+            /// private fields;
+            /// </summary>
+            private readonly IBookStorage bookStorage;
+            private List<Book> books = new List<Book>();
+          
+
+
+
+
+            /// <summary>
+            /// Ctor for bookStorage's initialization
+            /// </summary>
+            /// <param name="bookStorage"></param>
+            public BookService(IBookStorage bookStorage)
+            {
+                if (ReferenceEquals(bookStorage, null))
+                {
+                    throw new ArgumentException();
+                }
+                this.bookStorage = bookStorage;
+
+            }
+
+
+
+            public void AddBook(Book book)
+            {
+                if (ReferenceEquals(book, null))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                books.Add(book);
+
+            }
+
+
+            public void RemoveBookFromShop(Book book)
+            {
+                if (ReferenceEquals(book, null))
+                {
+                    throw new ArgumentNullException();
+                }
+                books.Remove(book);
+            }
+
+
+            public Book FindBook(IFinder parameter)
+            {
+                if (ReferenceEquals(parameter, null))
+                {
+                    throw new ArgumentNullException();
+                }
+                return parameter.FindBookByTeg();
+            }
+
+
+            public void Sort(IComparer<Book> comparator)
+            {
+                var booksArray = books.ToArray();
+
+                if (ReferenceEquals(comparator, null))
+                {
+                    Array.Sort(booksArray);
+                }
+                else
+                {
+                    Array.Sort(booksArray, comparator);
+                }
+                books.Clear();
+                books.AddRange(booksArray);
+            }
+
+
+            public void Save()
+            {
+                bookStorage.SaveBooks(books);
+            }
+
+
+            public IEnumerable<Book> GetAllBooks()
+            {
+                return bookStorage.GetBookList();
+            }
+
+
+
+
+        }
+    }
+
---
+++ BookListServiceLib/IBookService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookLib;
+using Finder;
+namespace BookListServiceLib
+{
+   public interface IBookService
+    {/// <summary>
+     /// Adds book to list
+     /// </summary>
+     /// <param name="book"></param>
+        void AddBook(Book book);
+
+        /// <summary>
+        /// Removes book from list
+        /// </summary>
+        /// <param name="book"></param>
+        void RemoveBookFromShop(Book book);
+
+        /// <summary>
+        /// Finds book in list
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        Book FindBook(IFinder parameter);
+
+        /// <summary>
+        /// Sorts by some teg
+        /// </summary>
+        /// <param name="comparator"></param>
+        void Sort(IComparer<Book> comparator);
+
+        /// <summary>
+        /// Saves information about books
+        /// </summary>
+        void Save();
+
+        /// <summary>
+        /// Returns list of books
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<Book> GetAllBooks();
+    }
+}
