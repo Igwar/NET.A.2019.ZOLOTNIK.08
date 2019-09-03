fc12d69d0faea64de39bc139bd632b06068a4542
Igwar <43217217+Igwar@users.noreply.github.com>
2019-09-03 15:19:48 +0300

    Add files via upload

---
+++ Logger/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+namespace Logger
+{
+    public class Logger : ILogger
+    {
+        private static readonly NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void Trace(string message) => logger.Trace(message);
+
+        public void Debug(string message) => logger.Debug(message);
+
+        public void Info(string message) => logger.Info(message);
+
+        public void Warn(string message) => logger.Warn(message);
+
+        public void Error(string message) => logger.Error(message);
+
+        public void Fatal(string message) => logger.Fatal(message);
+    }
+}
